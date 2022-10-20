c4e5c7394aae62fa6ee646c3f349098debff8a05
Marc <marcmoranm@gmail.com>
2022-10-20 13:53:45 +0200

    Primer avance del primer mínimo.

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -1,26 +1,55 @@
 import java.util.List;
 
 public interface ProductManager {
+
+    // ----------------------------------------------------------------------------------------------------
+
+    // FUNCTIONS RELATED WITH PRODUCTS.
+
+    // > Function 1.
     public List<Product> productsByPrice();
+
+    // > Function 2.
     public List<Product> productsBySales();
-    public void addOrder(Order order);
-    public Order processOrder();
-    public List<Order> ordersByUser(String userId);
-    /////////////////////////////////////////////
-    ////////////////////////////////////////////
 
-
-
-
-    public void addUser(String s, String name, String surname);
+    // > Function 3.
     public void addProduct(String productId, String name, double price);
 
+    // > Function 4.
     public Product getProduct(String productId);
 
-    public int numUsers();
+    // > Function 5.
     public int numProducts();
 
+    // ----------------------------------------------------------------------------------------------------
+
+    // FUNCTIONS RELATED WITH ORDERS.
+
+    // > Function 6.
+    public void addOrder(Order order);
+
+    // > Function 7.
+    public Order processOrder();
+
+    // > Function 8.
+    public List<Order> ordersByUser(String userId);
+
+    // > Function 9.
     public int numOrders();
 
+    // > Function 10.
     public int numSales(String b001);
+
+    // ----------------------------------------------------------------------------------------------------
+
+    // FUNCTIONS RELATED WITH USERS.
+
+    // > Function 11.
+    public void addUser(String s, String name, String surname);
+
+    // > Function 12.
+    public int numUsers();
+
+    // ----------------------------------------------------------------------------------------------------
+
 }
--- src/main/java/ProductManagerImpl.java
+++ src/main/java/ProductManagerImpl.java
@@ -6,63 +6,72 @@ public class ProductManagerImpl implements ProductManager{
     List<Product> products;
     Queue<Product> orders;
 
+    // ----------------------------------------------------------------------------------------------------
+
+    // FUNCTIONS RELATED WITH PRODUCTS.
+
+    // > Function 1.
     @Override
-    public List<Product> productsByPrice() {
-        return null;
-    }
+    public List<Product> productsByPrice() { return null; }
 
+    // > Function 2.
     @Override
-    public List<Product> productsBySales() {
-        return null;
-    }
+    public List<Product> productsBySales() { return null; }
 
+    // > Function 3.
     @Override
-    public void addOrder(Order order) {
+    public void addProduct(String productId, String name, double price) {}
 
-    }
+    // > Function 4.
+    @Override
+    public Product getProduct(String productId) { return null; }
 
+    // > Function 5.
+    @Override
+    public int numProducts() { return 0; }
+
+    // ----------------------------------------------------------------------------------------------------
+
+    // FUNCTIONS RELATED WITH ORDERS.
+
+    // > Function 6.
+    @Override
+    public void addOrder(Order order) {}
+
+    // > Function 7.
     @Override
     public Order processOrder() {
         return null;
     }
 
+    // > Function 8.
     @Override
     public List<Order> ordersByUser(String userId) {
         return null;
     }
 
+    // > Function 9.
     @Override
-    public void addUser(String s, String name, String surname) {
+    public int numOrders() { return 0; }
 
-    }
-
+    // > Function 10.
     @Override
-    public void addProduct(String productId, String name, double price) {
+    public int numSales(String b001) { return 0; }
 
-    }
+    // ----------------------------------------------------------------------------------------------------
 
+    // FUNCTIONS RELATED WITH USERS.
+
+    // > Function 11. public void addUser(String s, String name, String surname);
     @Override
-    public Product getProduct(String productId) {
-        return null;
-    }
+    public void addUser(String s, String name, String surname) {}
 
+    // > Function 12. public int numUsers();
     @Override
     public int numUsers() {
         return 0;
     }
 
-    @Override
-    public int numProducts() {
-        return 0;
-    }
+    // ----------------------------------------------------------------------------------------------------
 
-    @Override
-    public int numOrders() {
-        return 0;
-    }
-
-    @Override
-    public int numSales(String b001) {
-        return 0;
-    }
 }
